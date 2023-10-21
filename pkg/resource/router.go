dd7e95d94cd5fb838719a23c51fb3009275a689e
ZwangaMukwevho <zwangatm3@gmail.com>
2023-10-21 18:14:37 +0200

    none: refactor to use layered architecture

---
+++ pkg/resource/router.go
@@ -0,0 +1,24 @@
+package resource
+
+import (
+	"context"
+	"doc-classification/pkg/service"
+	"net/http"
+
+	"golang.org/x/oauth2"
+)
+
+// Retrieve a token, saves the token, then returns the generated client.
+func GetClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+
+	tokFile := "token.json" // to-do: have a unique one for each user
+	tok, err := service.TokenFromFile(tokFile)
+	if err != nil {
+		tok = service.GetTokenFromWeb(config)
+		service.SaveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
