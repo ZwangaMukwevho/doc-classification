a681262aff92e9997cdb88a5585728f581b3b86d
ZwangaMukwevho <zwangatm3@gmail.com>
2023-10-23 23:56:29 +0200

    fix: added ability to read the subject and attachment

--- main.go
+++ main.go
@@ -33,16 +33,49 @@ func main() {
 	}
 
 	user := "me"
-	r, err := srv.Users.Labels.List(user).Do()
+	// r, err := srv.Users.Labels.List(user).Do()
+	// if err != nil {
+	// 	log.Fatalf("Unable to retrieve labels: %v", err)
+	// }
+	// if len(r.Labels) == 0 {
+	// 	fmt.Println("No labels found.")
+	// 	return
+	// }
+	// fmt.Println("Labels:")
+	// for _, l := range r.Labels {
+	// 	fmt.Printf("- %s\n", l.Name)
+	// }
+
+	messages, err := srv.Users.Messages.List(user).Q("category:primary").MaxResults(8).Do()
 	if err != nil {
-		log.Fatalf("Unable to retrieve labels: %v", err)
+		log.Fatalf("Unable to retrieve unread messages: %v", err)
 	}
-	if len(r.Labels) == 0 {
-		fmt.Println("No labels found.")
-		return
+
+	fmt.Println("Subject headings of the 5 most recent emails:")
+	for _, message := range messages.Messages {
+		msg, err := srv.Users.Messages.Get(user, message.Id).Do()
+		if err != nil {
+			log.Printf("Unable to retrieve message details: %v", err)
+			continue
+		}
+		headers := msg.Payload.Headers
+		for _, header := range headers {
+			if header.Name == "Subject" {
+				fmt.Printf("- %s\n", header)
+				break
+			}
+		}
+
+		// Check for attachments
+		parts := msg.Payload.Parts
+		if parts != nil {
+			for _, part := range parts {
+				if part.Filename != "" {
+					fmt.Printf("  - Attachment Filename: %s\n", part.Filename)
+				}
+			}
+		}
+
 	}
-	fmt.Println("Labels:")
-	for _, l := range r.Labels {
-		fmt.Printf("- %s\n", l.Name)
-	}
+
 }
