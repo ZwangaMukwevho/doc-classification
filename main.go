747feda59ee4f53d65ddab26d9800af686663f22
ZwangaMukwevho <zwangatm3@gmail.com>
2024-09-11 21:49:09 +0200

    save cleaning up code

--- main.go
+++ main.go
@@ -96,14 +96,12 @@ func cronJob() {
 		if err != nil {
 			log.Fatalf("Unable to get gmail client: %v", err)
 		}
-		fmt.Printf("gmail client: %v \n", gmailClient)
 
 		// initialise the gmail service
 		srv, err := gmail.NewService(ctx, option.WithHTTPClient(gmailClient))
 		if err != nil {
 			log.Fatalf("Unable to retrieve Gmail client: %v", err)
 		}
-		fmt.Printf("gmail srv: %v \n", srv)
 
 		// Setting up the user and the time stamp
 		user := "me"
@@ -124,12 +122,10 @@ func cronJob() {
 		if err != nil {
 			log.Fatalf("Unable to get gmail client: %v", err)
 		}
-		fmt.Printf("drive client: %v \n", driveClient)
 		driveSrv, err := drive.NewService(ctx, option.WithHTTPClient(driveClient))
 		if err != nil {
 			log.Fatalf("Unable to retrieve Drive client: %v", err)
 		}
-		fmt.Printf("UnDrive service %v \n", driveClient)
 		localDriveService := service.DriveServiceLocal{Service: driveSrv}
 
 		/*
@@ -185,7 +181,7 @@ func setupCron() {
 	// Schedule the job to run every minute
 	// */3 * * * * fixing
 	// 0 0 * * * normal
-	_, err := c.AddFunc("0 0 * * *", cronJob)
+	_, err := c.AddFunc("*/13 * * * *", cleanUpCron)
 	if err != nil {
 		log.Println("Error scheduling cron job:", err)
 		return
@@ -195,4 +191,67 @@ func setupCron() {
 
 	// Run the cron scheduler in the background
 	select {}
+}
+
+func cleanUpCron() {
+	ctx := context.Background()
+	b, err := os.ReadFile("client_secret_973692223612-28ae9a7njdsfh7gv89l0fih5q36jt52m.apps.googleusercontent.com.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	firebaseDB, err := repository.InitDB("https://react-getting-started-78f85-default-rtdb.firebaseio.com", "firebase_service.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	firebaseRepository := repository.NewFirebaseRestClient(firebaseDB)
+
+	users, err := firebaseRepository.GetUserDataList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("users %v: \n", users)
+
+	for _, dbUserData := range *users {
+		// Google drive setup
+		driveConfig, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+		if err != nil {
+			log.Fatalf("Unable to parse client secret file to config: %v", err)
+		}
+
+		driveClient, err := resource.GetClientFromDBToken(driveConfig, dbUserData.GdriveCode, firebaseRepository, dbUserData.UserId)
+		if err != nil {
+			log.Fatalf("Unable to get gmail client: %v", err)
+		}
+		driveSrv, err := drive.NewService(ctx, option.WithHTTPClient(driveClient))
+		if err != nil {
+			log.Fatalf("Unable to retrieve Drive client: %v", err)
+		}
+		localDriveService := service.DriveServiceLocal{Service: driveSrv}
+
+		// Get current time and calculate the time for 2 days ago
+		twoDaysAgo := time.Now().AddDate(0, 0, -2).Format(time.RFC3339)
+		for categoryID, _ := range dbUserData.Categories {
+			// List the files in the current category (assuming categoryID is a folder ID)
+			r, err := localDriveService.Service.Files.List().
+				Q("'" + categoryID + "' in parents and modifiedTime > '" + twoDaysAgo + "'").
+				Fields("files(id, name, modifiedTime)").Do()
+
+			if err != nil {
+				log.Fatalf("Unable to retrieve files: %v", err)
+			}
+
+			for _, file := range r.Files {
+				// Delete the file
+				err := localDriveService.Service.Files.Delete(file.Id).Do()
+				if err != nil {
+					log.Printf("Unable to delete file: %v", err)
+				} else {
+					log.Printf("Deleted file: %s (%s)", file.Name, file.Id)
+				}
+			}
+		}
+	}
+
 }
